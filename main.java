175a3ac206c7009cca771cef195657799f9fa5ac
GaCasado <91262704+GaCasado@users.noreply.github.com>
2021-09-23 15:23:47 +0200

    Add files via upload

---
+++ main.java
@@ -0,0 +1,51 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class main {
+
+	private int ej;
+	private int numCartas;
+	private static OutputStream outFile = null;
+	private static InputStream inFile = null;
+	
+	public main(String[] args) {
+		try {			
+			start(args);
+		} catch (Exception e) {
+			System.err.println("Something went wrong ...");
+			System.err.println();
+			e.printStackTrace();
+		}
+	}
+	
+	private void start(String[] args) throws Exception {
+		parseArgs(args);
+		switch (ej) {
+		case 1:
+			
+			break;
+		case 2:
+			
+			break;
+		case 3:
+	
+			break;
+		case 4:
+	
+			break;
+
+		default:
+			System.err.println("Something went wrong ...");
+			throw new Exception();
+		}
+	}
+	
+	private void parseArgs(String[] args) throws FileNotFoundException {
+		ej = Integer.parseInt(args[5]);
+		outFile = new FileOutputStream(args[7]);
+		inFile = new FileInputStream(args[6]);
+	}
+}
