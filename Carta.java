a72a356a597b95c4a8afa09a6e2d12a9ba6c08c1
GaCasado <91262704+GaCasado@users.noreply.github.com>
2021-09-23 14:29:38 +0200

    Add files via upload

---
+++ Carta.java
@@ -0,0 +1,18 @@
+
+public class Carta {
+	private char palo;
+	private char num;
+	
+	public Carta(char p, char n){
+		this.palo = p;
+		this.num = n;
+	}
+	
+	public char getPalo() {
+		return palo;
+	}
+	
+	public char getNum() {
+		return num;
+	}
+}
