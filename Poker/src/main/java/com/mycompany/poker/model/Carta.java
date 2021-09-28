59e37844022a8800b122ff9f96c8445f4292e61f
RempalagoMcWilson <72865314+RempalagoMcWilson@users.noreply.github.com>
2021-09-28 20:30:16 +0200

    dsa
    dasc

--- Poker/src/main/java/com/mycompany/poker/model/Carta.java
+++ Poker/src/main/java/com/mycompany/poker/model/Carta.java
@@ -24,7 +24,7 @@ public class Carta {
             else{
                 switch(c){
                     case 'A':
-                        ca = 1;
+                        ca = 14;
                     break;
                     case 'T':
                         ca = 10;
--- Poker/src/main/java/com/mycompany/poker/model/Mano.java
+++ Poker/src/main/java/com/mycompany/poker/model/Mano.java
@@ -35,42 +35,54 @@ public class Mano {
         Collections.sort(cartas, (Carta c1, Carta c2) -> c1.compareTo(c2));
     }
 
-    private void buscaJugadas(){
-        Map<Integer,Integer> repeticiones = new HashMap<Integer,Integer>();
-        //prueba
-        int colH = 0, colD = 0, colC = 0, colS = 0, escalera = 0, escaleraReal = 0;
-        boolean hueco = false;
+    private void buscaJugadas(){//PENSADO PARA 5 CARTAS CAMBIAR PARA 7
+        Map<Integer,Integer> repeticiones = new HashMap<>();
+        
+        int colH = 0, colD = 0, colC = 0, colS = 0, escalera = 0,
+                escaleraC = 0, escaleraH = 0, escaleraD = 0, escaleraS = 0;
+        boolean hueco = false, color = false;
+        char po = 'z';
         
         for(int i = 0; i < nC - 1; i++){
+            
             if(cartas.get(i+1).getNum() - cartas.get(i).getNum() == 0){ 
                 Integer aux1 = repeticiones.putIfAbsent(cartas.get(i).getNum(), 2);
                 if(aux1 != null)
                     repeticiones.put(cartas.get(i).getNum(), aux1++);   
             }
+            
             else if(cartas.get(i+1).getNum() - cartas.get(i).getNum() == 1){
                 if(cartas.get(i).getPalo() == cartas.get(i+1).getPalo()){
-                    //escalera real
-                    escaleraReal++;
+                    if(po == 'z'){
+                        po = cartas.get(i).getPalo();
+                    }
+                    else{
+                        if(po != cartas.get(i).getPalo())
+                            ;
+                    }
+                
                 }
                 escalera++;
                 //contar escalera
             }
+            
             else{
                 if(cartas.get(i+1).getNum() - cartas.get(i).getNum() == 2 && !hueco){
                     hueco = true;
                     if(cartas.get(i).getPalo() == cartas.get(i+1).getPalo()){
                         //escalera real
-                        escaleraReal++;
+                        //escaleraReal++;
                     }
                     escalera++;
                 }
                 else {
                     escalera = 0;
-                    escaleraReal = 0;
+                    //escaleraReal = 0;
                     hueco = false;
                 }
                 //ver si se queda de proyecto de escalera por la mitad
             }
+            
             if(cartas.get(i+1).getPalo() == cartas.get(i).getPalo()){
                 switch(cartas.get(i).getPalo()) {
                     case 'h':
@@ -87,9 +99,11 @@ public class Mano {
                     break;
                 }
             }
+            
+            //GUARDAR LAS ESCALERAS QUE VAYAS TENIENDO y sus proyectos
         }
         
-        //tratar las repeticiones y gurdar las jugadas
+        //tratar las repeticiones si no hay escalera de color y gurdar la mejor jugada y los proectos de colr
     }
     public String jugadaToString(){
         return "hola";
