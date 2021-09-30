e7b27e689eaa308ef49d8ae7b1eeed12e5a91bbb
RempalagoMcWilson <72865314+RempalagoMcWilson@users.noreply.github.com>
2021-09-30 15:29:07 +0200

    a
    a

--- Poker/src/main/java/com/mycompany/poker/model/Mano.java
+++ Poker/src/main/java/com/mycompany/poker/model/Mano.java
@@ -8,7 +8,9 @@ package com.mycompany.poker.model;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Map;
-import java.util.HashMap;
+import java.util.TreeMap;
+import java.util.List;
+import java.util.ArrayList;
 //import java.lang.String;
 import java.util.List;
 import java.util.Comparator;
@@ -32,57 +34,27 @@ public class Mano {
                     , aux2 = entrada.charAt(i*2 + 1);
             cartas.add(new Carta(aux1, aux2));
         }
-        Collections.sort(cartas, (Carta c1, Carta c2) -> c1.compareTo(c2));
+        //Collections.sort(cartas, (Carta c1, Carta c2) -> c1.compareTo(c2));
     }
 
     private void buscaJugadas(){//PENSADO PARA 5 CARTAS CAMBIAR PARA 7
-        Map<Integer,Integer> repeticiones = new HashMap<>();
-        
-        int colH = 0, colD = 0, colC = 0, colS = 0, escalera = 0,
-                escaleraC = 0, escaleraH = 0, escaleraD = 0, escaleraS = 0;
-        boolean hueco = false, color = false;
-        char po = 'z';
-        
+        Map<Carta,Integer> repeticiones = new TreeMap<>();
+        List<Carta> lista = new ArrayList<>();
+        int colH = 0, colD = 0, colC = 0, colS = 0;//, escalera = 0,
+               // escaleraC = 0, escaleraH = 0, escaleraD = 0, escaleraS = 0;
+        boolean hueco = false;//, color = false;
+        //char po = 'z';
+        int pareja = -1, trio = -1, poker = -1, cont=0;
+        //String fullhouse = "-1";
+       
         for(int i = 0; i < nC - 1; i++){
             
             if(cartas.get(i+1).getNum() - cartas.get(i).getNum() == 0){ 
-                Integer aux1 = repeticiones.putIfAbsent(cartas.get(i).getNum(), 2);
+                Integer aux1 = repeticiones.putIfAbsent(cartas.get(i), 2);
                 if(aux1 != null)
-                    repeticiones.put(cartas.get(i).getNum(), aux1++);   
+                    repeticiones.put(cartas.get(i), aux1++);   
             }
-            
-            else if(cartas.get(i+1).getNum() - cartas.get(i).getNum() == 1){
-                if(cartas.get(i).getPalo() == cartas.get(i+1).getPalo()){
-                    if(po == 'z'){
-                        po = cartas.get(i).getPalo();
-                    }
-                    else{
-                        if(po != cartas.get(i).getPalo())
-                            ;
-                    }
-                
-                }
-                escalera++;
-                //contar escalera
-            }
-            
-            else{
-                if(cartas.get(i+1).getNum() - cartas.get(i).getNum() == 2 && !hueco){
-                    hueco = true;
-                    if(cartas.get(i).getPalo() == cartas.get(i+1).getPalo()){
-                        //escalera real
-                        //escaleraReal++;
-                    }
-                    escalera++;
-                }
-                else {
-                    escalera = 0;
-                    //escaleraReal = 0;
-                    hueco = false;
-                }
-                //ver si se queda de proyecto de escalera por la mitad
-            }
-            
+           
             if(cartas.get(i+1).getPalo() == cartas.get(i).getPalo()){
                 switch(cartas.get(i).getPalo()) {
                     case 'h':
@@ -103,9 +75,80 @@ public class Mano {
             //GUARDAR LAS ESCALERAS QUE VAYAS TENIENDO y sus proyectos
         }
         
+        for( Map.Entry<Carta, Integer>  r: repeticiones.entrySet()){
+            Carta auxC = r.getKey();
+            int auxR = r.getValue();
+            
+            lista.add(auxC);
+            
+            if(auxR == 2){
+                if(auxC.getNum() > pareja)
+                    pareja = auxC.getNum();
+            }
+            if(auxR == 3){
+                if(auxC.getNum() > trio)
+                    trio = auxC.getNum();
+            }
+            if(auxR == 4){
+                if(auxC.getNum() > poker)
+                    poker = auxC.getNum();
+            }
+            cont++;
+        }
+        
+        for(Carta c : lista){
+            
+        }
+        
+        
+        if(pareja != -1 && trio != -1){
+            //fullhouse
+        }
         //tratar las repeticiones si no hay escalera de color y gurdar la mejor jugada y los proectos de colr
     }
     public String jugadaToString(){
         return "hola";
     }
 }
+/*
+
+ 
+            else if(cartas.get(i+1).getNum() - cartas.get(i).getNum() == 1){
+                if(i > 4){
+                    
+                }
+                else{
+                    if(cartas.get(i).getPalo() == cartas.get(i+1).getPalo()){
+                    if(po == 'z'){
+                        po = cartas.get(i).getPalo();
+                    }
+                    else{
+                        if(po != cartas.get(i).getPalo())
+                            ;
+                    }
+                
+                }
+                escalera++;
+                //contar escalera
+                }   
+                
+            }
+            
+            else{
+                if(cartas.get(i+1).getNum() - cartas.get(i).getNum() == 2 && !hueco){
+                    hueco = true;
+                    if(cartas.get(i).getPalo() == cartas.get(i+1).getPalo()){
+                        //escalera real
+                        //escaleraReal++;
+                    }
+                    escalera++;
+                }
+                else {
+                    escalera = 0;
+                    //escaleraReal = 0;
+                    hueco = false;
+                }
+                //ver si se queda de proyecto de escalera por la mitad
+            }
+            
+*/
